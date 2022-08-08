98bd67fb49a76fc2d56763d8cb0830ae59a585dd
Marya Samsonenko <samashushka7@gmail.com>
2022-08-08 15:13:27 +0300

    add lesson 135

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
@@ -6,25 +7,54 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 import "./app.css";
 
-function App() {
-  const data = [
-    { name: "Степан Бандера", salary: "2000$", increase: true, id: 1 },
-    { name: "Михайло Крашко", salary: "900$", increase: false, id: 2 },
-    { name: "Олексій Коваль", salary: "1800$", increase: true, id: 3 },
-    { name: "Павло Ткач", salary: "800$", increase: false, id: 4 },
-    { name: "Ігор Петренко", salary: "1100$", increase: false, id: 5 },
-  ];
+class App extends Component {
+  constructor(prors) {
+    super();
+    this.state = {
+      data: [
+        { name: "Степан Бандера", salary: "2000$", increase: false, id: 1 },
+        { name: "Михайло Крашко", salary: "900$", increase: false, id: 2 },
+        { name: "Олексій Коваль", salary: "1800$", increase: false, id: 3 },
+        { name: "Павло Ткач", salary: "800$", increase: false, id: 4 },
+        { name: "Ігор Петренко", salary: "1100$", increase: false, id: 5 },
+      ],
+    };
+    this.maxId = 6;
+  }
 
-  return (
-    <div className="app">
-      <AppInfo />
-      <div className="search-panel">
-        <SearchPanel />
-        <AppFilter />
-        <EmployeesList data={data} />
-        <EmployeesAddForm />
+  deleteItem = (id) => {
+    this.setState(({ data }) => {
+      return { data: data.filter((item) => item.id !== id) };
+    });
+  };
+
+  addItem = (name, salary) => {
+    const newItem = {
+      name,
+      salary,
+      increase: false,
+      id: this.maxId++,
+    };
+    this.setState(({ data }) => {
+      const newArray = [...data, newItem];
+      console.log(newArray);
+      return { data: newArray };
+    });
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="app">
+        <AppInfo />
+        <div className="search-panel">
+          <SearchPanel />
+          <AppFilter />
+          <EmployeesList data={data} onDelete={this.deleteItem} />
+          <EmployeesAddForm onAdd={this.addItem} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 export default App;
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,10 +1,16 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete }) => {
   const items = data.map((item) => {
     const { id, ...itemProps } = item;
-    return <EmployeesListItem key={id} {...itemProps} />;
+    return (
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+      />
+    );
     // {...itemProps} = name={item.name} salary={item.salary}
   });
   return <ul className="app-list list-group">{items}</ul>;
